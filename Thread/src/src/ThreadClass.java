7840e070501ed14bbc0c70de0973b10c39c6f1ce
Rakesh Gupta <98424033+Rak-Code@users.noreply.github.com>
2025-01-03 20:53:46 +0530

    m

--- Thread/src/src/ThreadClass.java
+++ Thread/src/src/ThreadClass.java
@@ -29,21 +29,74 @@ public class ThreadClass {
 			}
 		}
 		
-		Implementor im = new Implementor();
-		im.start();
-		Implementor im2 = new Implementor();
-		im2.start();
-		Implementor im3 = new Implementor();
-		im3.start();
-		Implementor im4 = new Implementor();
-		im4.start();
+//		Implementor im = new Implementor();
+//		im.start();
+//		Implementor im2 = new Implementor();
+//		im2.start();
+//		Implementor im3 = new Implementor();
+//		im3.start();
+//		Implementor im4 = new Implementor();
+//		im4.start();
 		
+		
+		class Runa implements Runnable{
+
+			@Override
+			public void run() {
+				new Simple();
+				
+			}
+			
+		}
 //		Simple obj = new Simple();
 //		Simple obj2 = new Simple();
 //		Simple obj3 = new Simple();
 //		Simple obj4 = new Simple();
 		
 
+//		Thread t3 = new Thread(()->{
+//			new Simple();
+//		});
+//		
+//		t3.start();
+//		
+//		
+//		Thread t4 = new Thread(()->{
+//			new Simple();
+//		});
+//		
+//		t4.start();
+//		
+		Thread t5 = new Thread(()->{
+			for(int i =0;i<6;i++) {
+				count();
+			}
+		});
+	
+		
+		Thread t51 = new Thread(()->{
+			for(int i =0;i<6;i++) {
+				count();
+			}
+		});
+		
+		
+		Thread t52 = new Thread(()->{
+			for(int i =0;i<6;i++) {
+				count();
+			}
+		});
+		t5.start();
+		t51.start();
+		t52.start();
+		
 	}
 
+	
+	
+	static int count = 0;
+	synchronized static void count() {
+		count++;
+		System.out.println(count);
+	}
 }
